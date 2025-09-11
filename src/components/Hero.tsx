fbce4ac39b5fae1132e116fd0cdd0f85e5a29009
PaulAk5 <142666722+PaulAk5@users.noreply.github.com>
2025-09-11 18:04:20 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import heroImage from "@/assets/hero-car.jpg";
 
 export const Hero = () => {
   const openGoogleForm = () => {
-    window.open('https://forms.zohopublic.com/akintundepau123gm1/form/DriverWaitlist/formperma/ur-ILS3OJpU7bOP4-_zeQ12J1IUDt4f_7-zNjZA0xE', '_blank');
+    window.open('https://forms.zohopublic.com/akintundepau123gm1/form/DriverWaitlist/formperma/ur-ILS3OJpU7bOP4-_zeQ12J1IUDt4f_7-zNjZZA0xE');
   };
 
   return (
